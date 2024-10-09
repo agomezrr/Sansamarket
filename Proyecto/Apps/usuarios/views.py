4feb468504fc03e20d37ba6aa7dc3e00182bede5
agomezrr <agomezr@usm.cl>
2024-10-08 22:53:23 -0300

    Se agrega la app Usuarios

--- Proyecto/Apps/usuarios/views.py
+++ Proyecto/Apps/usuarios/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from .forms import RegistroForm
 
+from django.contrib.auth.decorators import login_required
+from .forms import PerfilForm
+from .models import Perfil
 # Create your views here.
+
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('perfil')
+    else:
+        form = RegistroForm()
+    return render(request, 'usuarios/registro.html', {'form': form})
+
+def actualizar_perfil(request):
+    perfil, created = Perfil.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = PerfilForm(request.POST, request.FILES, instance=perfil)
+        if form.is_valid():
+            form.save()
+            return redirect('perfil')
+    else:
+        form = PerfilForm(instance=perfil)
+    
+    return render(request, 'usuarios/perfil.html', {'form': form})
