7dafbd1da13eb533f0aaf06fc9cd1497e4e2e448
agomezrr <agomezr@usm.cl>
2024-10-23 10:35:36 -0300

    Se avanza en productos

--- Proyecto/Apps/productos/models.py
+++ Proyecto/Apps/productos/models.py
@@ -5,7 +5,7 @@ class Producto(models.Model):
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
     def __str__(self):
         return self.nombre
 
